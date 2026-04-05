0fef959d9fb7bd7ebb7f6b8df2e52b381ef43f35
damaoooo <damaoooo@163.com>
2026-04-05 17:23:38 +0300

    fix flowchart bug

--- IDA_scripts/IDA_flowchart/cli_flowchart.py
+++ IDA_scripts/IDA_flowchart/cli_flowchart.py
@@ -34,7 +34,9 @@
 ##############################################################################
 
 import click
+import shutil
 import subprocess
+import tempfile
 import multiprocessing
 from tqdm import tqdm
 
@@ -44,6 +46,7 @@ from os import getenv
 from os import walk
 from os.path import abspath
 from os.path import dirname
+from os.path import isdir
 from os.path import isfile
 from os.path import join
 from os.path import relpath
@@ -52,6 +55,9 @@ IDA_PATH = getenv("IDA_PATH", "/home/damaoooo/ida-pro-9.1/idat")
 IDA_PLUGIN = join(dirname(abspath(__file__)), 'IDA_flowchart.py')
 REPO_PATH = dirname(dirname(dirname(abspath(__file__))))
 LOG_PATH = "flowchart_log.txt"
+CSV_HEADER = (
+    "idb_path,fva,func_name,start_ea,end_ea,bb_num,bb_list,hashopcodes"
+)
 
 
 def execute_command(cmd):
@@ -62,6 +68,27 @@ def execute_command(cmd):
     return proc.returncode == 0
 
 
+def merge_partial_csvs(partial_csvs, output_csv):
+    """Merge temporary partial CSV files into a single output file."""
+    header_written = False
+    with open(output_csv, "w") as out_fp:
+        for partial_csv in partial_csvs:
+            if not isfile(partial_csv):
+                continue
+
+            with open(partial_csv, "r") as in_fp:
+                for line_idx, line in enumerate(in_fp):
+                    if line_idx == 0:
+                        if header_written:
+                            continue
+                        header_written = True
+                    out_fp.write(line)
+
+        if not header_written:
+            # Keep CSV output valid even when there are no input IDBs.
+            out_fp.write(CSV_HEADER + "\n")
+
+
 @click.command()
 @click.option("-i", "--idbs-folder", required=True,
               help="Path to the IDBs folder")
@@ -69,6 +96,7 @@ def execute_command(cmd):
               help="Path to the output CSV file")
 def main(idbs_folder, output_csv):
     """Call IDA_flowchart.py IDA script."""
+    temp_output_dir = None
     try:
         if not isfile(IDA_PATH):
             print("[!] Error: IDA_PATH:{} not valid".format(IDA_PATH))
@@ -78,7 +106,13 @@ def main(idbs_folder, output_csv):
         print("[D] IDBs folder: {}".format(idbs_folder))
         print("[D] Output CSV: {}".format(output_csv))
 
+        temp_output_dir = tempfile.mkdtemp(
+            prefix="flowchart_parts_",
+            dir=dirname(abspath(output_csv)))
+        print("[D] Temporary CSV folder: {}".format(temp_output_dir))
+
         commands = []
+        partial_csvs = []
 
         success_cnt, error_cnt = 0, 0
         for root, _, files in walk(idbs_folder):
@@ -99,41 +133,59 @@ def main(idbs_folder, output_csv):
                     join(getcwd(), root, f_name),  # absolute path if IDB
                     REPO_PATH)  # absolute path of the repo folder
 
+                partial_csv = join(
+                    temp_output_dir,
+                    "flowchart_part_{:08d}.csv".format(len(commands)))
+
                 cmd = [IDA_PATH,
                        '-A',
                        '-L{}'.format(LOG_PATH),
                        '-S{}'.format(IDA_PLUGIN),
                        '-Oflowchart:{}:{}'.format(
                            rel_idb_path,
-                           output_csv),
+                           partial_csv),
                        idb_path]
 
                 # print("[D] cmd: {}".format(' '.join(cmd)))
                 commands.append(cmd)
+                partial_csvs.append(partial_csv)
 
-        bar = tqdm(total=len(commands), desc="Processing IDBs")
-        pool = multiprocessing.Pool(processes=multiprocessing.cpu_count())
-        
-        rets = []
-        for cmd in commands:
-            p = pool.apply_async(execute_command, args=(cmd,), callback=lambda x: bar.update(1))
-            rets.append(p)
-            
-        pool.close()
-        pool.join()
-        
-        for p in rets:
-            if p.get():
-                success_cnt += 1
-            else:
-                error_cnt += 1
-                
+        merged_partials = []
+        if commands:
+            bar = tqdm(total=len(commands), desc="Processing IDBs")
+            pool = multiprocessing.Pool(processes=multiprocessing.cpu_count())
+
+            rets = []
+            for cmd in commands:
+                p = pool.apply_async(
+                    execute_command,
+                    args=(cmd,),
+                    callback=lambda x: bar.update(1))
+                rets.append(p)
+
+            pool.close()
+            pool.join()
+
+            for partial_csv, p in zip(partial_csvs, rets):
+                if p.get():
+                    success_cnt += 1
+                    merged_partials.append(partial_csv)
+                else:
+                    error_cnt += 1
+        else:
+            print("[!] Warning: no IDBs found in {}".format(idbs_folder))
+
+        merge_partial_csvs(merged_partials, output_csv)
 
         print("\n# IDBs correctly processed: {}".format(success_cnt))
         print("# IDBs error: {}".format(error_cnt))
+        print("[D] Merged CSV written to: {}".format(output_csv))
 
     except Exception as e:
         print("[!] Exception in cli_flowchart\n{}".format(e))
+    finally:
+        if temp_output_dir and isdir(temp_output_dir):
+            shutil.rmtree(temp_output_dir)
 
 
 if __name__ == '__main__':
